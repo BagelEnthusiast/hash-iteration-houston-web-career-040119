3c12b57b959f5ed41144c23298f82b5a5f642389
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-29 17:45:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,9 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if 
+    if name < 13
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
 end
 
